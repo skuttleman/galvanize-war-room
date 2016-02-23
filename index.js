c2fad2ba417b593ab4de54d35cc409807bb9ebe9
Ben Allred <skuttleman@gmail.com>
2016-02-23 09:56:29 -0700

    Title displayed in app

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
+try {
+  require('dotenv').load();
+} catch(error) {
+  console.log(error);
+}
 var express = require('express'), app = express();
+var server = require('http').Server(app);
+var port = process.env.PORT || 8000;
+
+app.use(express.static(__dirname + '/public'));
+
+server.listen(port, function() {
+  console.log('Server is listening on port', port);
+});
+
+
+// Error Handling
+if (process.env.NODE_ENV === 'development') {
+  app.use(function(error, request, response, next) {
+    response.status(error.status || 500);
+    response.json({
+      message: error.message,
+      error: error
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(error, request, response, next) {
+  response.status(error.status || 500);
+  response.json({
+    message: error.message,
+    error: {}
+  });
+});
